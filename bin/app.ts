a2a8657ce19e3aa172a481b2c81071e55e71a4f2
Tom Wright <tom.suffern.wright@gmail.com>
2023-10-04 16:41:25 +1100

    feat: application

--- bin/app.ts
+++ bin/app.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
+import { LoadBalancerStack } from "../lib/stack";
 
 const app = new cdk.App();
+new LoadBalancerStack(app, 'testing-multi-az', {
+  disableCrossZoneConnectivity: false
+})
---
+++ lib/stack.ts
@@ -0,0 +1,95 @@
+import * as cdk from "aws-cdk-lib";
+import { Cluster } from "aws-cdk-lib/aws-ecs";
+import { ApplicationLoadBalancer, ApplicationProtocol } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { TestingFargateService } from "./service";
+import { AclCidr, AclTraffic, Action, IVpc, NetworkAcl, SecurityGroup, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
+
+interface LoadBalancerStackProps extends cdk.StackProps {
+  disableCrossZoneConnectivity: boolean,
+}
+
+export class LoadBalancerStack extends cdk.Stack {
+  constructor(app: cdk.App, id: string, props: LoadBalancerStackProps) {
+    super(app, id, props);
+
+    const vpc = new Vpc(this, "Vpc", {
+      natGateways: 0, // NATs have an hourly cost
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+      maxAzs: 2,
+    })
+
+    if (props.disableCrossZoneConnectivity) {
+      this.disableCrossZoneConnectivity(vpc)
+    }
+
+
+    const cluster = new Cluster(this, "Cluster", {
+      clusterName: id,
+      vpc
+    });
+
+    const securityGroup = new SecurityGroup(this, "SecurityGroup", {
+      vpc,
+    });
+
+
+    const loadBalancer = new ApplicationLoadBalancer(this, "LoadBalancer", {
+      vpc,
+      securityGroup,
+      loadBalancerName: id,
+      internetFacing: true,
+    });
+
+    const service = new TestingFargateService(this, "Service", {
+      cluster,
+      securityGroup,
+    })
+
+    loadBalancer.addListener("Listener", {
+      protocol: ApplicationProtocol.HTTP,
+      defaultTargetGroups: [service.targetGroup]
+    })
+  }
+
+  private disableCrossZoneConnectivity(vpc: IVpc) {
+    const blockAtoB = new NetworkAcl(this, "BlockCrossZoneAtoB", {
+      vpc,
+    })
+    blockAtoB.addEntry("BlockAToB", {
+      ruleNumber: 100,
+      cidr: AclCidr.ipv4(vpc.publicSubnets[1].ipv4CidrBlock),
+      traffic: AclTraffic.allTraffic(),
+      ruleAction: Action.DENY,
+    })
+    blockAtoB.addEntry("AllowAll", {
+      ruleNumber: 999,
+      cidr: AclCidr.anyIpv4(),
+      traffic: AclTraffic.allTraffic(),
+      ruleAction: Action.ALLOW,
+    })
+    vpc.publicSubnets[0].associateNetworkAcl("BlockCrossZoneAtoB", blockAtoB)
+
+    const blockBtoA = new NetworkAcl(this, "BlockCrossZoneBtoA", {
+      vpc,
+    })
+    blockBtoA.addEntry("BlockBtoA", {
+      ruleNumber: 100,
+      cidr: AclCidr.ipv4(vpc.publicSubnets[0].ipv4CidrBlock),
+      traffic: AclTraffic.allTraffic(),
+      ruleAction: Action.DENY,
+    })
+    blockBtoA.addEntry("AllowAll", {
+      ruleNumber: 999,
+      cidr: AclCidr.anyIpv4(),
+      traffic: AclTraffic.allTraffic(),
+      ruleAction: Action.ALLOW,
+    })
+    vpc.publicSubnets[1].associateNetworkAcl("BlockCrossZoneBtoA", blockAtoB)
+  }
+}
